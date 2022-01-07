4cef3cfc41558beafe576720fca38d14933f8d4d
AkshaySalamwade <93402417+AkshaySalamwade@users.noreply.github.com>
2022-01-07 18:12:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const cors = require("cors");
+app.use(cors());
+
+const { adduser, selectAlluser } = require("./user");
+
+app.get("/users", async ( req, res) => 
+
+{
+
+    const list = await selectAlluser();
+    res.json(list);
+
+}
+
+);
+
+app.post("/add-user", async (req, res) =>
+
+{
+
+
+    const message = req.body;
+    await adduser(message);
+
+    res.json({message:"data added successfully in database"});
+});
+
+app.listen(4000, () => console.log("server started successfully"));
