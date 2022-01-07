154b0a550bafb200143b3941c08d7b36a205ae2a
AkshaySalamwade <93402417+AkshaySalamwade@users.noreply.github.com>
2022-01-07 18:21:49 +0530

    Add files via upload

---
+++ database/user.js
@@ -0,0 +1,44 @@
+const mysql = require("mysql");
+const Promise = require("bluebird");
+Promise.promisifyAll(require("mysql/lib/Connection").prototype);
+
+const dbinfo = {
+
+host: "localhost",
+user: "root",
+password: "cdac",
+database: "paracticeset",
+
+}
+
+const selectAlluser = async() => {
+
+const connection = mysql.createConnection(dbinfo);
+
+await connection.connectAsync();
+
+let sql = 'select * from message';
+const list = await connection.queryAsync(sql);
+
+await connection.endAsync();
+return list;
+
+};
+
+const adduser = async (message) => {
+
+const connection = mysql.createConnection(dbinfo);
+
+await connection.connectAsync();
+
+
+let sql = `INSERT INTO message (username,password,mobile,address) values ("akshay","arya","8652678794","navi-mumbai")`;
+
+connection.queryAsync(sql,[message.username, message.password, message.mobile, message.address]);
+console.log("Record added");
+
+await connection.endAsync();
+};
+
+
+module.exports = {selectAlluser, adduser};
